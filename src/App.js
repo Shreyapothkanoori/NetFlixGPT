2a8d0fb6b41b8f193ce06583a7d8b7f13911b188
Shreya POTHKANOORI <shreya.pothkanoori@hexagon.com>
2024-07-10 00:15:26 +0530

    cra & tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-color-green">
+       This is Netflix GPT APP
+      </header>
+    </div>
+  );
+}
+
+export default App;
